dc9d9a93944de4e5dd523386a14cbdbaac2e41ba
Gavin Grant <gavingrant@gmail.com>
2023-02-21 12:56:03 -0800

    fix: center modal on mobile

--- components/AppForm/Modal.vue
+++ components/AppForm/Modal.vue
@@ -17,7 +17,7 @@
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
         <div
-          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+          class="flex min-h-full items-center justify-center p-4 text-center sm:p-0"
         >
           <TransitionChild
             as="template"

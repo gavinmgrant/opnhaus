aadec403f6487ab75beeda86c0e3600074b89232
Gavin Grant <gavingrant@gmail.com>
2023-02-21 19:50:39 -0800

    chore: add google analytics

---
+++ plugins/vue-gtag.client.js
@@ -0,0 +1,10 @@
+import VueGtag, { trackRouter } from "vue-gtag-next";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: process.env.GOOGLE_ANALYTICS,
+    },
+  });
+  trackRouter(useRouter());
+});

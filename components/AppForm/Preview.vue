5e4a53e8d53e08bec2f63447a597cf2c0948117f
Gavin Grant <gavingrant@gmail.com>
2023-02-21 20:51:18 -0800

    refactor: mobile size of preview

--- components/AppForm/Preview.vue
+++ components/AppForm/Preview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen w-screen lg:w-auto flex justify-center items-center">
     <div
-      class="h-[650px] w-[304px] sm:h-[750px] sm:w-[350px] overflow-y-auto rounded-[2.5rem] ring-8 ring-slate-800 overflow-hidden"
+      class="h-[625px] w-[292px] md:h-[675px] md:w-[315px] xl:h-[750px] xl:w-[350px] overflow-y-auto rounded-[2.5rem] ring-8 ring-slate-800 overflow-hidden"
     >
       <templates-simple :acc="data" />
     </div>
